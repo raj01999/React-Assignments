7cf2ab3c80f20939d344408c8c80999a0439cd90
SARAFRAJ MALLICK <92747387+raj01999@users.noreply.github.com>
2022-04-01 19:58:37 +0530

    Assignment-3

---
+++ Assignment-3/code/src/component/CustomerInfo.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function OrderInfo() {
+  return (
+    <div className="customer-info">
+      <p>
+        <span>Appointment </span> 9:00 (24-05-2016)
+      </p>
+      <p>
+        <span>Email: </span> alan.form@email.nl
+      </p>
+      <p>
+        <span>Phone </span> +31123456789
+      </p>
+    </div>
+  );
+}
